0d0c3ab1da2e7d295a6a40d9adef28611f4b7d98
errdyy <errdao@gmail.com>
2023-06-07 21:46:02 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int size = 4; 
+
+        int[,] matrix = new int[size, size];
+
+        CompleteMatrix(matrix);
+
+        Console.WriteLine("Заполненная матрица:");
+        PrintMatrix(matrix);
+
+        Console.ReadLine();
+    }
+
+    static void CompleteMatrix(int[,] matrix)
+    {
+        int size = matrix.GetLength(0);
+
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                if (i == j)
+                {
+                    matrix[i, j] = i + 1;
+                }
+
+                if (i + j == size - 1)
+                {
+                    matrix[i, j] = j + 1; 
+                }
+            }
+        }
+    }
+
+    static void PrintMatrix(int[,] matrix)
+    {
+        int size = matrix.GetLength(0);
+
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                Console.Write(matrix[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
